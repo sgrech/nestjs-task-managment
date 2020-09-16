0efd1dabf5a980a9c9cd44a7b8c7d5092204b2d4
Shane Grech <shanegrech@gmail.com>
2020-09-16 17:21:01 +0200

    First Commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,60 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Patch,
+  Body,
+  Param,
+  Query,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    console.log(filterDto, 'status' in filterDto, 'search' in filterDto);
+    if ('status' in filterDto || 'search' in filterDto) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  // Method 1: get entire body
+  /* createTask(@Body() body) { */
+  // Method 2: get body parameters
+  /* createTask( */
+  /*   @Body('title') title: string, */
+  /*   @Body('description') description: string, */
+  /* ): Task { */
+  // Method 3: Use a DTO
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+}
